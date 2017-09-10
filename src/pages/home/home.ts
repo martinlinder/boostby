e8ebf13666c05dec79454b9eab8100e85916de8f
Philip <Philip.zdb@gmail.com>
2017-09-10 12:34:49 +0200

    Adds object used in view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,20 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  activity: {
+    image: string,
+    title: string,
+    info: string
+  };
 
   constructor(public navCtrl: NavController) {
-
+    this.getActivity()
   }
-
+  getActivity(): void {
+    this.activity = {
+      image: "http://lorempixel.com/400/300",
+      title: "Bada i Havsbadet",
+      info: "wrqprwqrhqwrhwqrhwqrpwq"
+    }
+  }
 }
