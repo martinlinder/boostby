8eacc33cb2ef5216e26c8571a8c216fc8c4af529
martinlinder <martin.liiinder@gmail.com>
2017-11-10 18:17:00 +0100

    Activity card (#1)
    * Adds initial grid
    
    * Finish initial draft
    
    * Adds object used in view
    
    * Adds distance to home activity card.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,22 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  activity: {
+    image: string,
+    title: string,
+    info: string,
+    distance: number
+  };
 
   constructor(public navCtrl: NavController) {
-
+    this.getActivity()
   }
-
+  getActivity(): void {
+    this.activity = {
+      image: "http://lorempixel.com/400/300",
+      title: "Bada i Havsbadet",
+      info: "beep boop",
+      distance: 5
+    }
+  }
 }
